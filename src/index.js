f2490e5e918a68f60d23de1acb1dd4cd3b241e80
paxxous <itsenveehio@gmail.com>
2022-02-05 17:50:42 -0500

    tweaked a few things and added an exit feature

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+
+// Create the main window
+const createWindow = () => {
+
+  // Adjust a few settings
+  const win = new BrowserWindow({
+    width: 400,
+    height: 400
+    
+  });
+
+  win.loadFile('index.html');
+}
+
+// Create window on ready so that no errors happen
+app.whenReady().then(() => {
+  createWindow();
+
+  // Global shortcut so the user has the ablitiy to exit
+  globalShortcut.register('ctrl+e', () => {
+    console.log("exiting...");
+    app.exit();
+  });
+});
+
+
+// when all windows close this app actually closes
+app.on('window-all-closed', () => {
+  if (process !== 'darwin') app.quit();
+})
+
