894d5eef96a4e401f1b189947988f8951b3c39ce
Paxxous <itsenveehio@gmail.com>
2022-02-12 08:02:18 -0500

    darkmode and advanced save mode :)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,53 @@
 /*
-  WARNING: THIS ISN'T THE NODEJS JAVASCRIPT,
-  DO NOT TRY AND WRITE NODE JS CODE IN HERE IT WILL NOT WORK.
-
-  lmao I'm pretty much arguing with myself now
+  Code being run by the html, the link between this, and 
+  the main index.js
 */
 
-const { ipcMain, ipcRenderer } = require('electron')
+
+const { ipcMain, ipcRenderer } = require('electron');
+const fs = require('fs');
 
 
-const getText = setInterval(function() {
-  var data = document.getElementById('the-editor').innerText
-  ipcRenderer.send('async', data)
-}, 1)
+
+
+// Save to file shortcut
+document.addEventListener('keydown', function(key) {
+  if (key.ctrlKey && key.key == 's') {
+
+    console.log('saving...');
+    let data = document.getElementById('the-editor').innerText;
+    ipcRenderer.send('save', data);
+
+  };
+});
+
+
+function getandsave() {
+  console.log('sent')
+  let text = document.getElementById('the-editor').innerText;
+
+  let nof = document.getElementById('nameOfFile').value;
+  let eof = document.getElementById('endOfFile').value;
+
+
+  ipcRenderer.send('saveCustom', text, nof, eof);
+  document.getElementById('bottombar').style.visibility = 'hidden';
+}
+
+
+
+// Advanced file save, basically the name of the file that you want to be saved
+document.addEventListener('keydown', (key) => {
+  if (key.ctrlKey && key.key == 'd') {
+    // Change visibility
+    document.getElementById('bottombar').style.visibility = 'visible';
+  }
+
+  // Atmempting to add darkmode
+  if (key.ctrlKey && key.key == 'g') {
+    console.log('Attempting change...');
+
+    document.getElementById('body').style.color = 'white';
+    document.getElementById('body').style.backgroundColor = 'rgb(56, 56, 56)';
+  }
+});
